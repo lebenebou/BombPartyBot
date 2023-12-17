8aadf81b89dadfd65497e17ad86d036057404a5a
lebenebou <lebenebou@gmail.com>
2023-12-17 20:36:49 +0200

    added BombPartyDict && ability to execute commands

--- main.py
+++ main.py
@@ -24,27 +24,75 @@ def handleFoundWord(word: str):
 def doNothing():
     pass
 
+def saveInvalidWords(invalidWords: list[str]):
+
+    with open(INVALID_WORDS_PATH, "a") as f:
+        f.write("\n".join(invalidWords))
+
+def safeExit(invalidWords: list[str]):
+
+    saveInvalidWords(invalidWords)
+    exit(0)
+
+def reset(session: GameSession):
+
+    session = GameSession(acceptedWords, handleFoundWord, doNothing, 3, 3)
+    os.system("cls")
+
+def showSessionInfo(session: GameSession):
+
+    print(f"{session.lastQueriedSubstring}: ", end="")
+
+    if session.lastFoundWord is None:
+        print("No word found")
+    else:
+        print(f"found wrod: {session.lastFoundWord}")
+    
+    for letter, weight in session.letterWeights.items():
+        
+        if weight == 0:
+            continue
+
+        print(letter, end="")
+
+    print("\n")
+
 if __name__ == "__main__":
 
     CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-    WORDS_FILE_PATH = os.path.join(CURRENT_DIR, "wordBank", "words60k.txt")
+    WORDS_PATH = os.path.join(CURRENT_DIR, "wordBank", "BombPartyDictionary.txt")
+    INVALID_WORDS_PATH = os.path.join(CURRENT_DIR, "wordBank", "invalidWords.txt")
 
     acceptedWords = []
-    with open(WORDS_FILE_PATH, "r") as f:
+    with open(WORDS_PATH, "r") as f:
         acceptedWords = f.read().splitlines()
 
     session = GameSession(acceptedWords, handleFoundWord, doNothing, 3, 3)
+
+    invalidWords = []
 
     os.system("cls")
 
     while(True):
 
-        if session.lastFoundWord is not None:
-            print(f"Last found word: {session.lastFoundWord}\n")
-        else:
-            print("No word found.\n")
+        showSessionInfo(session)
 
         substring: str = input("Enter the substring: ").replace(" ", "")
+
+        if substring == ":q":
+            safeExit()
+        
+        if substring == ":r":
+            reset(session)
+            continue
+
+        if substring == session.lastQueriedSubstring:
+            invalidWords.append(session.lastFoundWord)
+
+        if len(substring) > 3:
+            os.system("cls")
+            continue
+            
         session.queryOnSubstring(substring)
 
         os.system("cls")
